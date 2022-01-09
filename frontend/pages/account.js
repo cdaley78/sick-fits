dbfa6d1e59b727be0e5956cd052383039f03c393
Chris Daley <cjdaley@gmail.com>
2022-01-09 10:00:37 -0500

    init

---
+++ frontend/pages/account.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/display-name */
+export default function AccountPage() {
+  return (
+    <div>
+      <p>accounts page</p>
+    </div>
+  );
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+// export default function IndexPage() {
+//   return (
+//     // Ghost component
+//     <>
+//       <p>Hello!</p>
+//       <span>myspan</span>
+//     </>
+//   );
+// }
+
+// import component and export to display contents of another page on the home page
+// import ProductsPage from './products';
+// export default ProductsPage;
+
+// shortcut to the above
+export { default } from './products';
